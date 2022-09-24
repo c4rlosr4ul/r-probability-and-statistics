a202a01b25ad04f8b67bfd158466bd99b4a9b327
r4ulc4rlos <carlosraul.crlsrl@gmail.com>
2022-09-24 18:24:36 -0500

    v.3.0

--- hola_mundo.R
+++ hola_mundo.R
@@ -1,22 +1,25 @@
-	# If you dont have the packages string
-	# $ sudo -i R
-	# > install.packages("stringr")
-	# > install.packages("devtools")
-	# > devtools::install_github("tidyverse/stringr")	
+# If you dont have the packages string
+# $ sudo -i R
+# > install.packages("stringr")
+# > install.packages("devtools")
+# > devtools::install_github("tidyverse/stringr")	
 
-	library('stringr')
-	#that_is_a_commentariou XD
-	variable <- c("hola_csmares")
-	str_c(variable, collapse="")
-	variable <- c("hola_csmares tiene >>")
-	str_length(variable)
-	variable <- c("caracteres :P")
-	
-	#function that counts the number of characters 
-	#of the value in the variable
+library('stringr')
+#that_is_a_commentariou XD
+variable <- c("hola_csmares")
+var1 <- c("hola_csmares tiene >>")
+var2 <- c("caracteres :P")
 
-	#the_message	
-	#:D
+str_c(variable, collapse="")
+str_c(var1, collapse="")
+str_length(variable)
+str_c(var2, collapse="")
 
-	#If you want to run this code only put in the terminal
-	# $ Rscript hola_mundo.R
+#function that counts the number of characters 
+#of the value in the variable
+
+#the_message	
+#:D
+
+#If you want to run this code only put in the terminal
+# $ Rscript hola_mundo.R
